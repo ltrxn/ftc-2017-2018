a68fb4f98b2dff682b4b3b9e032417c8f85e0469
ltrxn <liwent22@gmail.com>
2017-11-08 17:56:18 -0500

    Switch operations to using a hardware class

--- AutonomousR1ColorDrive.java
+++ AutonomousR1ColorDrive.java
@@ -30,36 +30,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 public class AutonomousR1ColorDrive extends LinearOpMode {
 
+    HardwareIdeal robot = new HardwareIdeal();
 
-    //Wheel Motors
-    public DcMotor leftFront;
-    public DcMotor rightFront;
-    public DcMotor leftBack;
-    public DcMotor rightBack;
-
-    //Motor to pull up and down the glyph claw
-    public DcMotor glyphPully;
-
-    //Servos
-    public Servo glyphClawRight;
-    public Servo glyphClawLeft;
-    public Servo jewelKnockerRight;
-    public Servo jewelKnockerLeft;
-
-    //Vuforia
-    VuforiaLocalizer vuforia;
-
-    //Sensors
-    public ColorSensor rightColorSensor;
-    public ColorSensor leftColorSensor;
-    public GyroSensor sensorGyro;
-    public ModernRoboticsI2cGyro mrGyro;
-
-    //Values
-    static final double WHEEL_DIAMETER = 4.0; //In inches
-    static final int ANDYMARK_TICKS_PER_REV = 1120;
-    static final int TETRIX_TICK_PER_REV = 1440;
-    static final double TICKS_PER_INCH = ANDYMARK_TICKS_PER_REV / (WHEEL_DIAMETER * Math.PI); //Number of ticks in each inch (# of ticks in one rotation divided by the circumference of the wheel)
     double turnSpeed = .2;
     int zAccumulated; //total rotation left/right
     int rightCryptoboxDistance = 20;
@@ -74,49 +46,7 @@ public class AutonomousR1ColorDrive extends LinearOpMode {
          ********************* INITIALIZATION *********************
          **********************************************************/
 
-        //Declaring hardware
-        leftFront = hardwareMap.dcMotor.get("leftFront");
-        rightFront = hardwareMap.dcMotor.get("rightFront");
-        leftBack = hardwareMap.dcMotor.get("leftBack");
-        rightBack = hardwareMap.dcMotor.get("rightBack");
-        glyphPully = hardwareMap.dcMotor.get("glyphPully");
-
-        glyphClawLeft = hardwareMap.servo.get("glyphClawLeft");
-        glyphClawRight = hardwareMap.servo.get("glyphClawRight");
-        jewelKnockerLeft = hardwareMap.servo.get("jewelKnockerLeft");
-        jewelKnockerRight = hardwareMap.servo.get("jewelKnockerRight");
-
-        rightColorSensor = hardwareMap.colorSensor.get("colorRihgt");
-        leftColorSensor = hardwareMap.colorSensor.get("colorLeft");
-        sensorGyro = hardwareMap.gyroSensor.get("gyro");
-        mrGyro = (ModernRoboticsI2cGyro) sensorGyro;
-
-        //Setting up motors
-        rightFront.setDirection(DcMotor.Direction.REVERSE); //reverse the
-        rightBack.setDirection(DcMotor.Direction.REVERSE);  //right motors
-
-
-        //Vuforia Setup
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-        parameters.vuforiaLicenseKey = "ASqeker/////AAAAGXyaxZaNe0ngmcBSZn2x/aMgngJtpaXLUuvWMrmqLi768fIwOZ90EwGzH1f8bs6XPrz7WgklDoQCfX9QmfDMh6MxdyzlDpt5KFjmHrHrPgFi+qnZvD9qgfY4gTH8epM5T/tt7BJHwoNC4hjk9+Jf1Ane+XS6AXZrVf04wCynRUzE64zQDgGxflNMl73Q5qSd7BN3OvkAymgrQE4dsg1S97o7sX+obj+ubPZoJ7Hh8KriU6iOHmrVyTx6epZG2lWXDK0Iv6cQjku7Z5hvZNnK9Uvv8TRNIz5R71PBahS0nR4Xn0FH3beyMc+iu6ZNv33ZRuSh9MCPIUlzquqZtvMMPGbg3880nFmLkS9ytLn90kdX";
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
-        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        VuforiaTrackable relicTemplate = relicTrackables.get(0);
-        relicTemplate.setName("relicVuMarkTemplate"); // not necessary
-
-        //GRABBING THE GLYPH AT START
-        closeGlyphClaw();
-
-        //Calibrating mrGyro sensor
-        sleep(1000); //wait one second for gyro stop moving
-        mrGyro.calibrate();
-        while (mrGyro.isCalibrating()) {
-            //waiting for mrGyro sensor to finish calibrating
-        }
-
-
+        robot.init(hardwareMap);
 
         telemetry.addData(">", "Initialization finished. Press play to start");
         telemetry.update();
@@ -130,14 +60,14 @@ public class AutonomousR1ColorDrive extends LinearOpMode {
 
 
 
-        relicTrackables.activate();
+        robot.relicTrackables.activate();
 
         while (opModeIsActive()) {
 
             //Knock off the BLUE jewel
-            rightColorSensor.enableLed(true); //turn on color sensor
-            lowerJewelKnockerRight();
-            if(rightColorSensor.red()> rightColorSensor.blue()) { //if is to the right of the jewel
+            robot.turnOnColorSensorRight(); //turn on color sensor
+            robot.lowerJewelKnockerRight();
+            if(robot.rightColorSensor.red()> robot.rightColorSensor.blue()) { //if is to the right of the jewel
                 //move the robot right
                 gyroAbsoluteTurn(30);
                 gyroAbsoluteTurn(0);
@@ -149,22 +79,18 @@ public class AutonomousR1ColorDrive extends LinearOpMode {
 
             }
 
-
-            //vuforia - decrypts the pictograph
-            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
-
                 //if a pictograph is visible...
-            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-                telemetry.addData("VuMark", "%s visible", vuMark);
+            if (robot.getVuMark() != RelicRecoveryVuMark.UNKNOWN) {
+                telemetry.addData("VuMark", "%s visible", robot.getVuMark());
 
-                if (vuMark == RelicRecoveryVuMark.RIGHT) {
+                if (robot.getVuMark() == RelicRecoveryVuMark.RIGHT) {
                     //drive in front of the right column
                     scoreRight();
-                } else if (vuMark == RelicRecoveryVuMark.CENTER) {
+                } else if (robot.getVuMark() == RelicRecoveryVuMark.CENTER) {
                     //drive in front of the center column
                     scoreCenter();
 
-                } else if (vuMark == RelicRecoveryVuMark.LEFT) {
+                } else if (robot.getVuMark() == RelicRecoveryVuMark.LEFT) {
                     //drive in front of the left column
                     scoreLeft();
                 }
@@ -174,7 +100,7 @@ public class AutonomousR1ColorDrive extends LinearOpMode {
             }
 
             //Release the glyph ***UNSURE HOW THIS WILL WORK***
-            openGlyphClaw();
+            robot.openGlyphClaw();
 
             //ROBOT SHOULD BE IN SAFE ZONE, AUTONOMOUS OVER
 
@@ -188,57 +114,19 @@ public class AutonomousR1ColorDrive extends LinearOpMode {
     /**********************************************************
      ************************ METHODS**************************
      **********************************************************/
-
-    private void closeGlyphClaw() {
-        //Closes the glyph claw to grasp the block
-        double closedLeftGlyphClaw = .5;
-        double closedRightGlyphClaw = .5;
-
-        glyphClawLeft.setPosition(closedLeftGlyphClaw);
-        glyphClawRight.setPosition(closedRightGlyphClaw);
-    }
-
-    private void openGlyphClaw() {
-        //Open the glyph claw
-        double openLeftGlyphClaw = .1;
-        double openRightGlyphClaw = .9;
-
-        glyphClawLeft.setPosition(openLeftGlyphClaw);
-        glyphClawRight.setPosition(openRightGlyphClaw);
-    }
-
-    private void lowerJewelKnockerRight() {
-        //lowers jewel knocker right ***exact position yet to be determined***
-        jewelKnockerRight.setPosition(.95);
-    }
-
-    private void raiseJewelKnockerRight() {
-        //raises jewel knocker right ***exact position yet to be determined***
-        jewelKnockerRight.setPosition(.5);
-    }
-
-    private void lowerJewelKnockerLeft() {
-        //lowers jewel knocker left ***exact position yet to be determined***
-
-        jewelKnockerLeft.setPosition(.05);
-    }
-
-    private void raisesJewelKnockerLeft () {
-        //raises jewel knocker left ***exact position yet to be determined***
-
-        jewelKnockerLeft.setPosition(.5);
-    }
-
-
-
     //VUFORIA
     private void scoreRight() throws InterruptedException {
         /**score into right column
          * To Do:
-         * drive forward ~20" (rightCryptoboxDistance), turn 90 degrees right, drive forward ~12"
+         * drive until red line
+         * drive back ~4" (rightCryptoboxDistance), turn 90 degrees right, drive forward ~12"
          */
+        robot.turnOnColorSensorGround();
+        while (robot.groundColorSensor.alpha() < 10) {
+            robot.driveForward(.2);
+        }
 
-        encoderDrive(cryptoboxDrivePower, rightCryptoboxDistance); //Drive 20 inches at 0.5 power
+        robot.encoderDrive(cryptoboxDrivePower, -4); //Drive 20 inches at 0.5 power
 
         try {
             gyroTurn(90); // turn 90 degrees right
@@ -246,17 +134,23 @@ public class AutonomousR1ColorDrive extends LinearOpMode {
             telemetry.addData("GyroTurn: ", "Failed");// if GyroTurn fails, send message to driver station.
         }
 
-        encoderDrive(cryptoboxDrivePower, 12); //Drive 12 inches at 0.5 power;
+        robot.encoderDrive(cryptoboxDrivePower, 12); //Drive 12 inches at 0.5 power;
 
     }
 
     private void scoreCenter() throws InterruptedException {
         /**score into center column
          * To Do:
-         * drive forward ~28"(centerCryptoboxDistance), turn 90 degrees right, drive forward ~12"
+         * drive till red line
+         * drive forward ~2"(centerCryptoboxDistance), turn 90 degrees right, drive forward ~12"
          */
 
-        encoderDrive(cryptoboxDrivePower, centerCryptoboxDistance); //Drive 28 inches at 0.5 power;
+        robot.turnOnColorSensorGround();
+        while (robot.groundColorSensor.alpha() < 10) {
+            robot.driveForward(.2);
+        }
+
+        robot.encoderDrive(cryptoboxDrivePower, 2); //Drive 2 inches at 0.5 power
 
         try {
             gyroTurn(90); // turn 90 degrees right
@@ -264,17 +158,23 @@ public class AutonomousR1ColorDrive extends LinearOpMode {
             telemetry.addData("GyroTurn: ", "Failed");// if GyroTurn fails, send message to driver station.
         }
 
-        encoderDrive(cryptoboxDrivePower, 12); //Drive 12 inches at 0.5 power;
+        robot.encoderDrive(cryptoboxDrivePower, 12); //Drive 12 inches at 0.5 power;
 
     }
 
     private void scoreLeft() {
         /**score into left column
          * To Do:
-         * drive forward ~36" (leftCryptoboxDistance), turn 90 degrees right, drive forward ~12"
+         * drive till red line
+         * drive forward ~8" (leftCryptoboxDistance), turn 90 degrees right, drive forward ~12"
          */
 
-        encoderDrive(cryptoboxDrivePower, leftCryptoboxDistance); //Drive 36 inches at 0.5 power;
+        robot.turnOnColorSensorGround();
+        while (robot.groundColorSensor.alpha() < 10) {
+            robot.driveForward(.2);
+        }
+
+        robot.encoderDrive(cryptoboxDrivePower, 8); //Drive 8 inches at 0.5 power
 
         try {
             gyroTurn(90); // turn 90 degrees right
@@ -282,94 +182,38 @@ public class AutonomousR1ColorDrive extends LinearOpMode {
             telemetry.addData("GyroTurn: ", "Failed"); // if GyroTurn fails, send message to driver station.
         }
 
-        encoderDrive(cryptoboxDrivePower, 12); //Drive 12 inches at 0.5 power;
+        robot.encoderDrive(cryptoboxDrivePower, 12); //Drive 12 inches at 0.5 power;
 
     }
 
-    //DRIVING METHODS
-
-    public void driveForward(double power) {
-        //Drives forward. Parameters: power = how fast you want the robot to go
-        leftFront.setPower(power);
-        rightFront.setPower(power);
-        leftBack.setPower(power);
-        rightBack.setPower(power);
-    }
-
-    public void stopDriving() {
-        //Drives forward. Parameters: power = how fast you want the robot to go
-        leftFront.setPower(0);
-        rightFront.setPower(0);
-        leftBack.setPower(0);
-        rightBack.setPower(0);
-    }
-
-    public void encoderDrive(double power, double distance) {
-        //Drives forward. Parameters: power = how fast you want the robot to go, distance = how far (in inches)
-        int target;
-        target =  (int)(distance * TICKS_PER_INCH); //Multiply to find # of ticks to drive (then type cast to an int)
-
-        //reset encoders
-        leftFront.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        rightFront.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        leftBack.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        rightBack.setMode(DcMotor.RunMode.RESET_ENCODERS);
-
-        //set target position
-        leftFront.setTargetPosition(target);
-        rightFront.setTargetPosition(target);
-        leftBack.setTargetPosition(target);
-        rightBack.setTargetPosition(target);
-
-        //set to RUN_TO_POSITION mode
-        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        //set drive power
-        driveForward(power);
-
-        while(leftFront.isBusy() && rightFront.isBusy() && leftBack.isBusy() && rightBack.isBusy()) {
-            //wait until target position is reached
-        }
-
-        //stop and change modes back to normal
-        stopDriving();
-        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-    }
-
+    //TURN
     public void gyroTurn(int target) throws InterruptedException {
         //Use this method to turn. Parameter: target = how many degrees to turn.
-        gyroAbsoluteTurn(target + mrGyro.getIntegratedZValue());
+        gyroAbsoluteTurn(target + robot.mrGyro.getIntegratedZValue());
     }
 
     public void gyroAbsoluteTurn(int target) throws InterruptedException {
         //Turns robot to the target from where the robot started at
         //DO NOT USE THIS METHOD TO TURN
-        zAccumulated = mrGyro.getIntegratedZValue(); //set variable to gyro readings
+        zAccumulated = robot.mrGyro.getIntegratedZValue(); //set variable to gyro readings
         while (Math.abs(zAccumulated-target) > 3) { //run this loop if robot is 3 degrees or more away from target
             if (zAccumulated > target) {  //if gyro is positive, we will turn left
-                leftFront.setPower(-turnSpeed);
-                rightFront.setPower(turnSpeed);
-                leftBack.setPower(-turnSpeed);
-                rightBack.setPower(turnSpeed);
+                robot.leftFront.setPower(-turnSpeed);
+                robot.rightFront.setPower(turnSpeed);
+                robot.leftBack.setPower(-turnSpeed);
+                robot.rightBack.setPower(turnSpeed);
             }
 
             if (zAccumulated < target) {  //if gyro is negative, we will turn right
-                leftFront.setPower(turnSpeed);
-                rightFront.setPower(-turnSpeed);
-                leftBack.setPower(turnSpeed);
-                rightBack.setPower(-turnSpeed);
+                robot.leftFront.setPower(turnSpeed);
+                robot.rightFront.setPower(-turnSpeed);
+                robot.leftBack.setPower(turnSpeed);
+                robot.rightBack.setPower(-turnSpeed);
             }
             idle();
-            zAccumulated = mrGyro.getIntegratedZValue();  //Set variables to gyro readings
+            zAccumulated = robot.mrGyro.getIntegratedZValue();  //Set variables to gyro readings
         }
-        stopDriving();
+        robot.stopDriving();
 
         telemetry.addData("accu", String.format("%03d", zAccumulated));
         telemetry.update();
---
+++ HardwareIdeal.java
@@ -0,0 +1,266 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * Created by Trxn on 11/8/2017.
+ */
+
+public class HardwareIdeal {
+    //Wheel Motors
+    public DcMotor leftFront;
+    public DcMotor rightFront;
+    public DcMotor leftBack;
+    public DcMotor rightBack;
+
+    //Motor to pull up and down the glyph claw
+    public DcMotor glyphPully;
+
+    //Servos
+    public Servo glyphClawRight;
+    public Servo glyphClawLeft;
+    public Servo jewelKnockerRight;
+    public Servo jewelKnockerLeft;
+
+    //Vuforia
+    public VuforiaLocalizer vuforia;
+    public VuforiaTrackables relicTrackables;
+    public VuforiaTrackable relicTemplate;
+
+    //Sensors
+    public ColorSensor rightColorSensor;
+    public ColorSensor leftColorSensor;
+    public ColorSensor groundColorSensor;
+    public GyroSensor sensorGyro;
+    public ModernRoboticsI2cGyro mrGyro;
+
+
+    //Values
+    static final double WHEEL_DIAMETER = 4.0; //In inches
+    static final int ANDYMARK_TICKS_PER_REV = 1120;
+    static final double DRIVE_GEAR_REDUCTION = .5;     // We have geared up, so Gear Reduction < 1
+    static final int TETRIX_TICK_PER_REV = 1440;
+    static final double TICKS_PER_INCH = (TETRIX_TICK_PER_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER * Math.PI); //Number of ticks in each inch (# of ticks in one rotation divided by the circumference of the wheel)
+
+
+    //Others
+    public DcMotor.RunMode initialMode = null;
+    HardwareMap map = null;
+
+
+    public HardwareIdeal(DcMotor.RunMode enteredMode) {
+        initialMode = enteredMode;
+    }
+
+    public HardwareIdeal() {
+        this(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void init(HardwareMap aMap) throws InterruptedException {
+        map = aMap;
+
+        //Declaring hardware
+        leftFront = map.dcMotor.get("leftFront");
+        rightFront = map.dcMotor.get("rightFront");
+        leftBack = map.dcMotor.get("leftBack");
+        rightBack = map.dcMotor.get("rightBack");
+        glyphPully = map.dcMotor.get("glyphPully");
+
+        glyphClawLeft = map.servo.get("glyphClawLeft");
+        glyphClawRight = map.servo.get("glyphClawRight");
+        jewelKnockerLeft = map.servo.get("jewelKnockerLeft");
+        jewelKnockerRight = map.servo.get("jewelKnockerRight");
+
+        rightColorSensor = map.colorSensor.get("colorRihgt");
+        leftColorSensor = map.colorSensor.get("colorLeft");
+        groundColorSensor = map.colorSensor.get("colorGround");
+        sensorGyro = map.gyroSensor.get("gyro");
+        mrGyro = (ModernRoboticsI2cGyro) sensorGyro;
+
+
+        //ENCODERS
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //VUFORIA SETUP
+        int cameraMonitorViewId = map.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", map.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = "ASqeker/////AAAAGXyaxZaNe0ngmcBSZn2x/aMgngJtpaXLUuvWMrmqLi768fIwOZ90EwGzH1f8bs6XPrz7WgklDoQCfX9QmfDMh6MxdyzlDpt5KFjmHrHrPgFi+qnZvD9qgfY4gTH8epM5T/tt7BJHwoNC4hjk9+Jf1Ane+XS6AXZrVf04wCynRUzE64zQDgGxflNMl73Q5qSd7BN3OvkAymgrQE4dsg1S97o7sX+obj+ubPZoJ7Hh8KriU6iOHmrVyTx6epZG2lWXDK0Iv6cQjku7Z5hvZNnK9Uvv8TRNIz5R71PBahS0nR4Xn0FH3beyMc+iu6ZNv33ZRuSh9MCPIUlzquqZtvMMPGbg3880nFmLkS9ytLn90kdX";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+        relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // not necessary
+
+        //CALIBRATE GYROSENSOR
+        sleep(1000); //wait one second for gyro stop moving
+        mrGyro.calibrate();
+        while (mrGyro.isCalibrating()) {
+            //waiting for mrGyro sensor to finish calibrating
+        }
+
+        //ALL
+        leftFront.setMode(initialMode);
+        rightFront.setMode(initialMode);
+        leftBack.setMode(initialMode);
+        rightBack.setMode(initialMode);
+
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+    }
+
+
+    /**********************************************************
+     ************************ METHODS**************************
+     **********************************************************/
+
+    //VUFORIA
+    public RelicRecoveryVuMark getVuMark() {
+        //vuforia - decrypts the pictograph
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        return vuMark;
+    }
+    //CLAWS
+
+    public void closeGlyphClaw() {
+        //Closes the glyph claw to grasp the block
+        double closedLeftGlyphClaw = .5;
+        double closedRightGlyphClaw = .5;
+
+        glyphClawLeft.setPosition(closedLeftGlyphClaw);
+        glyphClawRight.setPosition(closedRightGlyphClaw);
+    }
+
+    public void openGlyphClaw() {
+        //Open the glyph claw
+        double openLeftGlyphClaw = .1;
+        double openRightGlyphClaw = .9;
+
+        glyphClawLeft.setPosition(openLeftGlyphClaw);
+        glyphClawRight.setPosition(openRightGlyphClaw);
+    }
+
+    public void lowerJewelKnockerRight() {
+        //lowers jewel knocker right ***exact position yet to be determined***
+        jewelKnockerRight.setPosition(.95);
+    }
+
+    public void raiseJewelKnockerRight() {
+        //raises jewel knocker right ***exact position yet to be determined***
+        jewelKnockerRight.setPosition(.5);
+    }
+
+    public void lowerJewelKnockerLeft() {
+        //lowers jewel knocker left ***exact position yet to be determined***
+
+        jewelKnockerLeft.setPosition(.05);
+    }
+
+    public void raisesJewelKnockerLeft() {
+        //raises jewel knocker left ***exact position yet to be determined***
+
+        jewelKnockerLeft.setPosition(.5);
+    }
+
+    //DRIVE
+
+    public void driveForward(double power) {
+        //Drives forward. Parameters: power = how fast you want the robot to go
+        leftFront.setPower(power);
+        rightFront.setPower(power);
+        leftBack.setPower(power);
+        rightBack.setPower(power);
+    }
+
+    public void stopDriving() {
+        //Drives forward. Parameters: power = how fast you want the robot to go
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+    }
+
+    public void encoderDrive(double power, double distance) {
+        //Drives forward. Parameters: power = how fast you want the robot to go, distance = how far (in inches)
+        int target;
+        target = (int) (distance * TICKS_PER_INCH); //Multiply to find # of ticks to drive (then type cast to an int)
+
+        //reset encoders
+        leftFront.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        rightFront.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        leftBack.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        rightBack.setMode(DcMotor.RunMode.RESET_ENCODERS);
+
+        //set target position
+        leftFront.setTargetPosition(target);
+        rightFront.setTargetPosition(target);
+        leftBack.setTargetPosition(target);
+        rightBack.setTargetPosition(target);
+
+        //set to RUN_TO_POSITION mode
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //set drive power
+        driveForward(power);
+
+        while (leftFront.isBusy() && rightFront.isBusy() && leftBack.isBusy() && rightBack.isBusy()) {
+            //wait until target position is reached
+        }
+
+        //stop and change modes back to normal
+        stopDriving();
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    //COLOR SENSOR
+    public void turnOnColorSensorRight() {
+        rightColorSensor.enableLed(true);
+    }
+
+    public void turnOffColorSensorRight() {
+        rightColorSensor.enableLed(false);
+    }
+
+    public void turnOnColorSensorLeft() {
+        leftColorSensor.enableLed(true);
+    }
+
+    public void turnOffColorSensorLeft() {
+        leftColorSensor.enableLed(false);
+    }
+
+    public void turnOnColorSensorGround() {
+        groundColorSensor.enableLed(true);
+    }
+
+    public void turnOffColorSensorGround() {
+        groundColorSensor.enableLed(false);
+    }
+}
